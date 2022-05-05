3d7228ec200e94325bc47df6785dcc791721220f
HemantTrellance <hkhinchi.trellance@gmail.com>
2022-05-05 16:07:14 +0530

    1 file modified

--- src/store/index.js
+++ src/store/index.js
@@ -1,29 +1,29 @@
 import { createStore } from 'redux';
 const redux = require('redux');
-const intionalStage = { counter: 0, showCounter = true};
+const intionalStage = { counter: 0, showCounter: true };
 const counterReducer = (state = intionalStage, action) => {
   if (action.type === 'increment') {
     return {
       counter: state.counter + 1,
-      showCounter:state.showCounter
+      showCounter: state.showCounter,
     };
   }
   if (action.type === 'increase') {
     return {
       counter: state.counter + action.demoNumber,
-      showCounter:state.showCounter
+      showCounter: state.showCounter,
     };
   }
   if (action.type === 'decrement') {
     return {
       counter: state.counter - 1,
-      showCounter:state.showCounter
+      showCounter: state.showCounter,
     };
   }
-  if(action.type === 'toggle'){
+  if (action.type === 'toggle') {
     return {
-      showCounter:!state.showCounter,
-      counter:state.counter
+      showCounter: !state.showCounter,
+      counter: state.counter,
     };
   }
   return state;
