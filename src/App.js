5efd6a6d188ecf007da9098857f8b19945a0e202
HemantTrellance <hkhinchi.trellance@gmail.com>
2022-05-10 17:45:00 +0530

    4 files modified

--- src/App.js
+++ src/App.js
@@ -1,14 +1,18 @@
 import React from 'react';
+import { useSelector } from 'react-redux';
 import Auth from './components/Auth';
 import Header from './components/Header';
 import Counter from './components/Counter';
+import UserProfile from './components/UserProfile';
 import './style.css';
 
 export default function App() {
+  const isAuth = useSelector((state) => state.auth.isAuthenticated);
   return (
     <>
       <Header />
-      <Auth />
+      {!isAuth && <Auth />}
+      {isAuth && <UserProfile />}
       <Counter />
     </>
   );
